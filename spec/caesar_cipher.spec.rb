5f900097784723d2d44df303036edc6a5c033a99
vitor0p9f <dev.vitorpfontes002@gmail.com>
2023-12-16 11:53:43 -0300

    refactor(spec/caesar_cipher.spec.rb): Renaming the test description.
    - From 'should shift letter to the right based on offset' to 'should shift a letter to the right from an offset'.

--- spec/caesar_cipher.spec.rb
+++ spec/caesar_cipher.spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 RSpec.describe 'Caesar cipher' do
   context 'Auxiliar functions' do
-    it 'should shift letter based on offset' do
+    it 'should shift a letter to the right from an offset' do
       shifted_letter = shift_letter('A', 2)
 
       expect(shifted_letter).to eq 'C'
