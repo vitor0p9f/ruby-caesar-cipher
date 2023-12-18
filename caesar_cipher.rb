0b1c12c12606f4ead8e1110a194298c5254f5c74
vitor0p9f <dev.vitorpfontes002@gmail.com>
2023-12-18 11:04:59 -0300

    feat(caesar_cipher.rb): Adding a check to the function shift_letter
    - Checking if the last letter of alphabet is being shifted to the right, if so, it returns to first letter.
    - Making the "should return to the beginning of the alphabet when 'Z' is shifted to the right" test pass.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,14 @@
 def shift_letter(letter, offset)
+  ascii_code = {
+    'A' => 65,
+    'a' => 97,
+    'Z' => 90,
+    'z' => 122
+  }
+
+  return (ascii_code['a'] + (offset - 1)).chr if (letter.ord + offset) > ascii_code['z']
+
+  return (ascii_code['A'] + (offset - 1)).chr if (letter.ord + offset) > ascii_code['Z']
+
   (letter.ord + offset).chr
 end
